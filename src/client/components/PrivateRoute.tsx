8cd89ca47cca55b3277c6fbad040f933971b4dfc
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2021-11-24 13:43:40 -0500

    client side auth ingration branch started, creating private routes

---
+++ src/client/components/PrivateRoute.tsx
@@ -0,0 +1,12 @@
+
+import * as React from 'react';
+
+const PrivateRoute = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+} 
+
+export default PrivateRoute
---
+++ src/client/views/HomeTest.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeTest = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HomeTest
